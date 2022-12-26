c2c2bdd356e2c956367de741cb54009963ef7363
norindes <norindes@gmail.com>
2022-12-25 20:46:55 -0600

    website 1.11

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -58,7 +58,7 @@ export type SectionId = typeof SectionId[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `Hi, I'm Gilberto Garcia`,
+  name: `Gilberto Garcia`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
