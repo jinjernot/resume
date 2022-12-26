b90c158209aa69540fdb9367a84808d99079e8c3
norindes <norindes@gmail.com>
2022-12-26 12:51:10 -0600

    Website 1.3

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -13,7 +13,7 @@ import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
+import porfolioImage from '../images/portfolio/portfolio.png';
 import profilepic from '../images/profilepic.png';
 import testimonialImage from '../images/testimonial.webp';
 import {
@@ -112,11 +112,11 @@ export const skills: SkillGroup[] = [
     skills: [
       {
         name: 'English',
-        level: 10,
+        level: 8.5,
       },
       {
         name: 'Spanish',
-        level: 10,
+        level: 9,
       },
     ],
   },
@@ -181,25 +181,25 @@ export const portfolioItems: PortfolioItem[] = [
     title: 'Sabarobe Studios Web page',
     description: 'React Web page for Sabarobe Studios',
     url: 'https://www.sabarobestudios.io/',
-    image: porfolioImage4,
+    image: porfolioImage,
   },
   {
     title: 'Pixel Homies',
     description: 'React Web page for Pixel Homies NFT Collection',
     url: 'https://www.pixelhomies.io/',
-    image: porfolioImage4,
+    image: porfolioImage,
   },
   {
     title: 'Boda Daniel & Cinthia',
     description: 'Wedding invitation Website',
     url: 'https://bodacinthiaydaniel.webflow.io/',
-    image: porfolioImage4,
+    image: porfolioImage,
   },
   {
     title: 'Instagram Scraper',
     description: 'Instagram Scraper Using Selenium',
     url: 'https://github.com/jinjernot/Jinjernot',
-    image: porfolioImage4,
+    image: porfolioImage,
   },
  
 ];
@@ -218,34 +218,117 @@ export const education: TimelineItem[] = [
 
 export const experience: TimelineItem[] = [
   {
-    date: 'Oct 2018 - Present',
+    date: '2018 - Present',
     location: 'HP Inc',
     title: 'Global Content Support',
     content: (
       <p>
-      • Immediate attention to critical cases on the content for product pages.
-      • Monitoring the flow of content (images, specs, descriptions, release dates) for the support pages.
-      • Publisher engagement and escalation management. 
-      • Management of content tools 
-      • Review of content for new products.
-      • Monitoring the performance of web pages (JavaScript, HTML, CSS)
-      • Interaction with IT to solve problems and improve assets
+      • Immediate attention to critical cases on the content for product pages.<br/>
+      • Monitoring the flow of content (images, specs, descriptions, release dates) for the support pages.<br/>
+      • Publisher engagement and escalation management.<br/>
+      • Management of content tools.<br/>
+      • Review of content for new products.<br/>
+      • Monitoring the performance of web pages.<br/>
+      • Interaction with IT to solve problems and improve assets<br/><br/>
+      <b>Skills:</b> Service Desk · Data Management · XML · Troubleshooting · Help Desk Support · Technical Support · Multitasking · Microsoft Office · Content Management · Data Analysis
       </p>
+
+      
     ),
   },
+
   {
-    date: 'March 2020 - Present',
+    date: '2020 - Present',
     location: 'Sabarobe Studios',
     title: 'FullStack Developer',
     content: (
       <p>
-      • Design and develop web applications using React.js
-      • Development of trading algorithms using PineScript and Python
-      • Development of  web scraping and automation using Selenium
-      • 3D Asset modeling for NFT collections and Three.js projects
+      • Design and develop web applications using React.js<br/>
+      • Development of trading algorithms using PineScript and Python<br/>
+      • Development of  web scraping and automation using Selenium<br/>
+      • 3D Asset modeling for NFT collections and Three.js projects<br/><br/>
+      <b>Skills:</b> Blender · Three.js · Jira · Web Development · Git · HTML · CSS · React.js · JavaScript · XML · Selenium · Data Science · Python · Multitasking · Web Design · Adobe Photoshop · Blockchain
+      </p>
+
+      
+    ),
+  },
+
+  {
+    date: '2017 - 2018',
+    location: 'HP Inc',
+    title: 'Quickspecs Specialist',
+    content: (
+      <p>
+      • Development of Quickspecs documents for Business Notebook and Desktop.<br/>
+      • Development of Datasheets documents for Commercial accesories.<br/><br/>
+      <b>Skills:</b> Data Management · Multitasking · Microsoft Office · Graphic Design · Content Management · Adobe Photoshop
       </p>
     ),
   },
+
+  {
+    date: '2015 - 2017',
+    location: 'Hewlett Packard Enterprise',
+    title: 'Video Upload Coordinator',
+    content: (
+      <p>
+    • Provide video upload and publishing services to ~100 HPE marketing stakeholders across regions.<br/>
+    • Manage video upload  and publishing to HPE Video Gallery / YouTube, input all necessary metadata, update status to stakeholders.<br/>
+    • Work with support team to fix technical issues.<br/>
+    • Track performance and issues.<br/>
+    • Meet standard TAT.<br/><br/>
+    <b>Skills:</b> Asset Management · Data Management · XML · Multitasking · Microsoft Office · Content Management · Project Management · Data Analysis
+      </p>
+    ),
+  },
+
+  {
+    date: '2015 - 2017',  
+    location: 'Hewlett Packard Enterprise',
+    title: 'Image Acquisition Lead',
+    content: (
+      <p>
+    • Ensure the automated delivery of product images and dynamic links in support of new product introductions and product updates.<br/>
+    • Engage with AMS, EMEA & APJ Businesses and Publishers to drive new business requirements, capabilities and processes that improve customer experience and end to end efficiency.<br/>
+    • Solve issues under tight deadlines, keeping track using a ticketing system to ensure customer satisfaction.<br/><br/>
+    <b>Skills:</b> Asset Management · Data Management · XML · Troubleshooting · Multitasking · Microsoft Office · Graphic Design · Content Management · Adobe Photoshop · Project Management
+      </p>
+    ),
+  },
+
+  {
+    date: '2013 - 2017',  
+    location: 'HP',
+    title: 'Product Catalog Operations',
+    content: (
+      <p>
+      • Management and publishing content for the HPE.com product catalog worldwide.<br/>
+      • Development of web solutions for internal HPE clients.<br/>
+      • Coordination and launch of new products for the HPE.com online catalog.<br/>
+      • Web solutions for internal clients worldwide.<br/>
+      • Perform daily end to end product content update status check to ensure each system works properly.<br/>
+      • Participated in migration project from SDL Tridion to Adobe Enterprise Manager.<br/><br/>
+    <b>Skills:</b> Web Development · HTML · JavaScript · Data Management · XML · Troubleshooting · Multitasking · Microsoft Office · Content Management
+      </p>
+    ),
+  },
+
+  {
+    date: '2011 - 2013',  
+    location: 'Red Movil',
+    title: 'Technical Support Engineer',
+    content: (
+      <p>
+      • Remote Network Monitoring  and proactive activities to ensure guests have the proper Internet service in hotels all across Mexico.<br/>
+      • Coordinate with local IT to fix problems in a fast way.<br/>
+      • Issue tracking using a ticketing system.<br/>
+      • Support  Coverage 24/7 via call center to help with issues related to the Internet Service.<br/><br/>
+      <b>Skills:</b> TCP/IP · Network Security · Service Desk · Troubleshooting · Help Desk Support · Technical Support · Multitasking · Microsoft Office
+      </p>
+    ),
+  },
+
 ];
 
 /**
