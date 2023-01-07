49dcea402d91a4432776e74c92257bc7c4fbabbd
norindes <norindes@gmail.com>
2023-01-06 18:11:50 -0600

    fixed again

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -13,7 +13,6 @@ const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
       <Head>
         <title>{title}</title>
         <meta content={description} name="description" />
-
         {/* several domains list the same content, make sure google knows we mean this one. */}
         <link href={`https://reactresume.com${pathname}`} key="canonical" rel="canonical" />
 
@@ -21,19 +20,20 @@ const Page: NextPage<HomepageMeta> = memo(({children, title, description}) => {
         <link href="/icon.svg" rel="icon" type="image/svg+xml" />
         <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
         <link href="/site.webmanifest" rel="manifest" />
-        <meta itemprop="image" content="%PUBLIC_URL%/logo1200.png" />
+        <meta content="%PUBLIC_URL%/logo1200.png" />
+        
 
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title" />
         <meta content={description} property="og:description" />
         <meta content={`https://reactresume.com${pathname}`} property="og:url" />
-        <meta itemprop="image" content="%PUBLIC_URL%/logo1200.png" />
+        <meta content="%PUBLIC_URL%/logo1200.png"/>
 
 
         {/* Twitter: https://developer.twitter.com/en/docs/twitter-for-websites/cards/overview/markup */}
         <meta content={title} name="twitter:title" />
         <meta content={description} name="twitter:description" />
-        <meta itemprop="image" content="%PUBLIC_URL%/logo1200.png" />
+        <meta content="%PUBLIC_URL%/logo1200.png" />
       </Head>
       {children}
     </>
